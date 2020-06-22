5fff8364d8d71ea2f07a6f43fe02725c067eb8df
cyrign <cyrign@cse.iitk.ac.in>
2020-06-22 23:29:27 +0530

    snakes and ladders completed

--- snake_and_ladders.cpp
+++ snake_and_ladders.cpp
@@ -13,10 +13,10 @@ struct Edge {
 class Compare
 {
 public:
-    bool operator() (pair<int, int>& a, pair<int, int>& b)
+    bool operator() (pair<int, pair<int, int>>& a, pair<int, pair<int, int>>& b)
     {
         
-        return(b.second-a.second < 0) ;
+        return(b.second.first < a.second.first);
     }
 };
 
@@ -43,6 +43,7 @@ class Graph {
             // head is s.first tail is s.second
             int head = s.first;
             int tail = s.second;
+            adjList[head].clear();
             adjList[head].push_back({tail,0});
         }
     }
@@ -53,31 +54,48 @@ class Graph {
             // head is s.first tail is s.second
             int bottom = s.first;
             int top = s.second;
+            adjList[bottom].clear();
             adjList[bottom].push_back({top,0});
         }
     }
     
     int findSolution(int N) {
         // PQ stores the index of the vertex and its current known shortest distance
-        priority_queue<pair<int, int>, std::vector<pair<int, int>>, Compare> pq; // min pq  b  (a>b)  
+        priority_queue<pair<int, pair<int, int>>, std::vector<pair<int, pair<int, int>>>, Compare> pq; // min pq  b  (a>b)  
         vector<bool> discovered(N+1, false);
         vector<int> distance(N+1, 1000000);
+        vector<int> parent(N+1, 0);
         distance[1] = 0;
 
         for (int i=1; i<=100; i++) {
-            pq.push({i, distance[i]});
+            pq.push({i, {distance[i], 0}});
         }
         
         int total_discovered = 0;
         // cout << pq.top().first << " " << pq.top().second << "\n";
         while(total_discovered<100){
-            pair<int, int> smallest = pq.top();
-            cout << smallest.first << " " << smallest.second << endl;
+            pair<int, pair<int, int>> smallest = pq.top();
+            if(discovered[smallest.first]){
+                pq.pop();
+                continue;
+            }
+            discovered[smallest.first]=true;
+            pq.pop();
+            distance[smallest.first]=smallest.second.first;
+            parent[smallest.first] = smallest.second.second;
+            if(smallest.first == 100) break;
+            for(auto child : adjList[smallest.first]){
+                pq.push({child.first,{distance[smallest.first]+child.second,smallest.first}});
+            }
             total_discovered += 1;
         }
         // do stuff while some nodes are not visited
 
-
+        int start = 100;
+        while(start != 1){
+            cout << start << " " << endl;
+            start = parent[start];
+        }
         return distance[100];
     }
 };
@@ -90,7 +108,7 @@ int main()
 	unordered_map<int, int> ladder, snake;
     Graph g(100);
 	// insert ladders into the map
-	ladder[1] = 38;
+	// ladder[1] = 38;
 	ladder[4] = 14;
 	ladder[9] = 31;
 	ladder[21] = 42;
@@ -111,7 +129,7 @@ int main()
     
     g.addLadders(ladder);
     g.addSnakes(snake);
-	g.findSolution(100);
+	cout << g.findSolution(100) << endl;
 
 	return 0;
 }
